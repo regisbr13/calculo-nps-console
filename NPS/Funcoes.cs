c68ffe8d6df4c973fd742f9b6120a01b0fbd096c
Regis Lima <regislima1391@gmail.com>
2019-02-21 22:18:20 -0300

    Programa concluido

---
+++ NPS/Funcoes.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPS
+{
+    class Funcoes
+    {
+        // Calculo da quantidade de clientes para uma pesquisa de NPS satisfatória
+        public static int Amostra(int totalClientes)
+        {
+            double x90 = Math.Pow(2.57 / 0.02, 2) * 0.5 * 0.5;
+            return (int)((totalClientes * x90) / (totalClientes + x90));
+        }
+
+        // Calculo da quantidade mínima de disparos com base em um aproveitamento de 25% de respostas
+        public static int Disparos(int amostra)
+        {
+            return (int)(amostra / 0.25);
+        }
+
+        // Função para calculo do NPS
+        public static int NPS(int d, int n, int p)
+        {
+            double total = d + n + p;
+
+            return (int)(100 * (p - d) / total);
+        }
+
+
+
+        // Função para calculo da variância
+        public static double VarNPS(int d, int n, int p)
+        {
+            int nps = NPS(d, n, p);
+            double total = d + n + p;
+
+            return d / total * Math.Pow((-1.0 - nps / 100.0), 2) + n / total * Math.Pow((-nps / 100.0), 2) + p / total * Math.Pow((1.0 - nps / 100.0), 2);
+        }
+
+        // Função para calculo da margem de erro
+        public static double MoeNPS(int d, int n, int p)
+        {
+            double total = d + n + p;
+            return 100 * Math.Sqrt(VarNPS(d, n, p) / total);
+        }
+
+        // Intervalo de confiança e margem de erro com base no nível de confiança
+        public static string IntervaloConfianca(int d, int n, int p)
+        {
+            double _90inf = NPS(d, n, p) - 1.64 * MoeNPS(d, n, p);
+            double _90sup = NPS(d, n, p) + 1.64 * MoeNPS(d, n, p);
+
+            double _95inf = NPS(d, n, p) - 1.96 * MoeNPS(d, n, p);
+            double _95sup = NPS(d, n, p) + 1.96 * MoeNPS(d, n, p);
+
+            double _99inf = NPS(d, n, p) - 2.57 * MoeNPS(d, n, p);
+            double _99sup = NPS(d, n, p) + 2.57 * MoeNPS(d, n, p);
+
+            return "Margem de erro: " + (1.64 * MoeNPS(d, n, p)).ToString("F2") + "% - " + "Intervalo para 90% de confiança: [" + (int)_90inf + "," + (int)_90sup + "]\n"
+                + "Margem de erro: " + (1.96 * MoeNPS(d, n, p)).ToString("F2") + "% - " + "Intervalo para 95% de confiança: [" + (int)_95inf + "," + (int)_95sup + "]\n"
+                + "Margem de erro: " + (2.57 * MoeNPS(d, n, p)).ToString("F2") + "% - " + "Intervalo para 99% de confiança: [" + (int)_99inf + "," + (int)_99sup + "]";
+        }
+    }
+}
--- NPS/Program.cs
+++ NPS/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace NPS
 {
@@ -6,7 +7,71 @@ namespace NPS
     {
         static void Main(string[] args)
         {
+            int opcao = 1;
+            while (opcao != 0)
+            {
+                try
+                {
+                    // Menu interativo
+                    Console.Clear();
+                    Console.WriteLine("0 - Sair");
+                    Console.WriteLine("1 - Saber tamanho mínimo da amostra e quantidade mínima de disparos");
+                    Console.WriteLine("2 - Indicadores sobre NPS");
+                    Console.Write("Escolha uma opção: ");
+                    opcao = int.Parse(Console.ReadLine());
+
+                    if (opcao == 1)
+                    {
+                        // Entrada de dados: total de clientes
+                        Console.Write("Total de clientes: ");
+                        int total = int.Parse(Console.ReadLine());
+                        
+                        // Saída de dados
+                        Console.Clear();
+                        Console.WriteLine("Você precisará de uma amostra de " + Funcoes.Amostra(total).ToString() + " clientes");
+                        Console.WriteLine("Também precisará de uma quantidade mínima de : " + Funcoes.Disparos(Funcoes.Amostra(total)).ToString() + " disparos");
+                        Console.WriteLine();
+
+                        Console.WriteLine("Digite qualquer tecla para voltar");
+                        Console.ReadLine();
+                    }
+                    else if (opcao == 2)
+                    {
+                        // Entrada de dados: detratores, passivos ou neutros e promotores
+                        Console.Write("Detratores: ");
+                        int det = int.Parse(Console.ReadLine());
+                        Console.Write("Neutros: ");
+                        int neut = int.Parse(Console.ReadLine());
+                        Console.Write("Promotores: ");
+                        int prom = int.Parse(Console.ReadLine());
+
+                        // Saída de dados
+                        Console.Clear();
+                        Console.WriteLine("NPS: " + Funcoes.NPS(det, neut, prom).ToString());
+                        Console.WriteLine("Variância: " + Funcoes.VarNPS(det, neut, prom).ToString("F2"));
+                        Console.WriteLine(Funcoes.IntervaloConfianca(det, neut, prom));
+
+                        Console.WriteLine();
+                        Console.WriteLine("Digite qualquer tecla para voltar");
+                        Console.ReadLine();
+                    }
+                    else if (opcao == 0)
+                    {
+                        Console.WriteLine("Fim do programa");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Opção inválida");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Erro inesperado: " + e.Message);
+                }
+              
+            }
             
+            Console.ReadKey();
         }
     }
 }
